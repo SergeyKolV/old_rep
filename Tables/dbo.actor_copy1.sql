49003fadd0fa72f6d3dfbe58e64b0bc4e8a22a5d
SergeyKolV <126674726+SergeyKolV@users.noreply.github.com>
2026-03-17 15:53:03 +0200

    Update dbo.actor_copy1.sql

--- Tables/dbo.actor_copy1.sql
+++ Tables/dbo.actor_copy1.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[actor_copy1] (
   [actor_id] [int] IDENTITY,
   [first_name] [varchar](45) NOT NULL,
-  [last_name] [varchar](45) NOT NULL,
+  [last_name] [varchar](45) NOT,
   [last_update] [datetime] NOT NULL CONSTRAINT [DC_actor_copy_last_update] DEFAULT (getdate()),
   [11111111111111111] [varchar](50) NULL,
   PRIMARY KEY NONCLUSTERED ([actor_id])
