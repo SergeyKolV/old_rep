ffa57a562dbb12bd2ed75457c97c2a09ebdd41fd
Serhii Kolesnyk <SergeyKol@datasoft.local>
2026-03-19 15:30:07 +0200

    1

---
+++ Tables/dbo.back__up_table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[back__up_table] (
+  [id] [int] NOT NULL,
+  [back_up] [varchar](50) NULL,
+  CONSTRAINT [PK_back__up_table_id] PRIMARY KEY CLUSTERED ([id])
+)
+ON [PRIMARY]
+GO
---
+++ Tables/dbo.dept.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[dept] (
+  [deptno] [decimal](2) NOT NULL,
+  [dname] [varchar](14) NULL,
+  [loc] [varchar](13) NULL,
+  CONSTRAINT [PK_dept_deptno] PRIMARY KEY CLUSTERED ([deptno]),
+  CONSTRAINT [UK_dept_deptno] UNIQUE ([deptno])
+)
+ON [PRIMARY]
+GO
+
+EXEC sys.sp_addextendedproperty N'MS_Description', N'Номер філії', 'SCHEMA', N'dbo', 'TABLE', N'dept', 'COLUMN', N'deptno'
+GO
+
+EXEC sys.sp_addextendedproperty N'MS_Description', N'Назва філії', 'SCHEMA', N'dbo', 'TABLE', N'dept', 'COLUMN', N'dname'
+GO
+
+EXEC sys.sp_addextendedproperty N'MS_Description', N'Місто, в якому розташована філія', 'SCHEMA', N'dbo', 'TABLE', N'dept', 'COLUMN', N'loc'
+GO
