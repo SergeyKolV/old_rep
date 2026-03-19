47235ed354f3eb5f86c93c2f4b3bf826700a18c7
SergeyKolV <126674726+SergeyKolV@users.noreply.github.com>
2026-03-19 15:31:02 +0200

    Delete Tables/dbo.back__up_table.sql

--- Tables/dbo.back__up_table.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[back__up_table] (
-  [id] [int] NOT NULL,
-  [back_up] [varchar](50) NULL,
-  CONSTRAINT [PK_back__up_table_id] PRIMARY KEY CLUSTERED ([id])
-)
-ON [PRIMARY]
-GO
