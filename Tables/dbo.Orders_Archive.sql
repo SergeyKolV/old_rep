a9ca09061bd2515139ab87e268ca5eb978a5d1a8
Serhii Kolesnyk <SergeyKol@datasoft.local>
2026-07-21 14:59:37 +0300

    123

---
+++ Tables/dbo.Orders_Archive.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Orders_Archive] (
+  [OrderID] [int] NOT NULL,
+  [OrderDate] [datetime] NOT NULL,
+  [CustomerID] [int] NOT NULL,
+  [Amount] [decimal](10, 2) NOT NULL,
+  CONSTRAINT [PK_Orders_Archive] PRIMARY KEY CLUSTERED ([OrderDate], [OrderID]) ON [PS_Orders_Date] ([OrderDate])
+)
+ON [PS_Orders_Date] ([OrderDate])
+GO
